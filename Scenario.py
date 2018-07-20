d2514dbf9927a4e1cc45c60c0060cab68fc6ee2c
antoniakormpa <41465615+antoniakormpa@users.noreply.github.com>
2018-07-20 14:21:37 +0300

    Update Scenario.py

--- Scenario.py
+++ Scenario.py
@@ -1,48 +1,146 @@
-# This example will test two of our Product's Analog outputs, and make sure one is lower than a particular threshold, and the other one higher.
+# This example will test the functionality of our device when we shut down the WiFi network. That's important because it's one of the core functionalities of our device, and it's one of the unique environmental inputs that our testing infrastructure allows when testing a product.
+
 #
-# The goal of this example is to show you how you can read a simple analog output from your device, and how you can make comparative assertions.
-#
-# In our particular example, we expect the pin we're connecting to our Testboard's A0 to be always less than a threshold value, and the pin we're connecting to our Testboard's A1 to be always greater than a threshold value. Of course this would never be a real world example, it's only for educational purposes
-#
-# If you want to replicate this setup, you can use our Particle Photon Testboard and connect the A0 pin to 3V3, and the A1 pin to GND.
+
+# In this particular example the device we are testing is a Smart Switch, which responds by toggling one of its pins On of Off whenever the user manually presses the button on the device..
+
+
+
+# Our Testboard is acting as the Access Point that provides network connectivity to our device. So by turning off the Access Point our device should get into Network Discovery mode. However, it should still operate properly.
+
+
+
+# The goal of this example is to show you how you can control more exotic (but absolutely critical) "inputs" in a test. Besides sensors, inputs and outputs, the network connectivity is an important part of the environment that needs to be mocked/tested in a functional test.
+
+
+
+# This is one real world example of a complex, but realistic, functional test you would run for your devices.
+
+
 
 import time
+
 from Spanner import Spanner
+
 from Testboard import Testboard
 
-TESTBOARD_ID = "250020001047343438323536"
+
+
+TESTBOARD_ID = "2c0019001347343438323536"
+
 testboard = Testboard(TESTBOARD_ID)
 
-# Our device's 1st Analog Output Pin will be connected to the Testboard's A0, making it our Input Pin 1
-INPUT_PIN_1 = "A4"
-# Our device's 2nd Analog Output Pin will be connected to the Testboard's A1, making it our Input Pin 2
-INPUT_PIN_2 = "A5"
 
-def validate_analog_input_greater():
-    # Check PIN state
-    # analogRead will give us a value between 0 to 4095, corresponding to a 0-3V3 range.
-    value = testboard.analogRead(INPUT_PIN_1)
 
-    # Let's say we want to to make sure the voltage is greater than 1.5V. Given the mapping of 0-3.3V to a value of 0-4096, that means the value we have should be higher than aproximately 1861. For the sake of simplicity and because of possible fluctuations in the values, we'll test with 1800, which is aprox. 1.45V.
-    # NOTICE: We could also have used analogReadVoltage() as we do in the next example.
-    spanner.assertGreaterThan(1800, value);
-    # See also assertGreatherThanOrEqual(), or assertEquals()
+# Our device's Output Pin will be connected to the Testboard's D7, making it our Input Pin
 
-def validate_analog_input_less():
-    # Check PIN state
-    # In this example, we use analogReadVoltage() which gives us a Voltage value directly, without having to care about the ADC converter. However, keep in mind that this value could be slightly less accurate, and given that it's a float it's not the best fit for checking Equality. Still, it's good enough for most purposes.
-    value = testboard.analogReadVoltage(INPUT_PIN_2)
-    print(value)
-    spanner.assertLessThan(2.0, value);
-    # See also assertLessThanOrEqual()
+INPUT_PIN = "D7"
+
+
+
+# Our Product's Input will be connected the Testboard's Pin D3, making it our Output Pin
+
+OUTPUT_PIN = "D3"
+
+
+
+def initialize_switch_off():
+
+    # check PIN state
+
+    switch_status = testboard.digitalRead(INPUT_PIN)
+
+
+
+    # If the switch is currently on, toggle the device's button (through our Testboards OUTPUT) 100 miliseconds, simulating a user input.
+
+    if(switch_status == 1):
+
+        testboard.toggle(OUTPUT_PIN, 100)
+
+
+
+    time.sleep(2)
+
+
+
+    # check PIN state again
+
+    switch_status = testboard.digitalRead(INPUT_PIN)
+
+
+
+    # If the switch is still on, we have some kind of problem so there's no point in running our tests. So we are exiting
+
+    if(switch_status == 1):
+
+        return 1
+
+
+
+def wififoff_toggle_button_toggle_switch():
+
+    # We are turning off the Access Point, so our device will now be in Network Discovery mode.
+
+    testboard.wifiOff()
+
+
+
+    time.sleep(10)
+
+
+
+    # check PIN state, make sure it's OFF
+
+    switch_status = testboard.digitalRead(INPUT_PIN)
+
+    spanner.assertFalse(switch_status)
+
+
+
+    # Simulate a real-life button press. We toggle the device's button (through our Testboards OUTPUT) 100 miliseconds
+
+    testboard.toggle(OUTPUT_PIN, 100)
+
+
+
+    # check PIN state, make sure it's now ON
+
+    switch_status = testboard.digitalRead(INPUT_PIN)
+
+    spanner.assertTrue(switch_status)
+
+
+
+    # Simulate a real-life button press. We toggle the device's button (through our Testboards OUTPUT) 100 miliseconds
+
+    testboard.toggle(OUTPUT_PIN, 100)
+
+
+
+    # check PIN state, make sure it's now OFF again
+
+    switch_status = testboard.digitalRead(INPUT_PIN)
+
+    spanner.assertFalse(switch_status)
+
+
+
+
 
 if __name__ == "__main__":
 
-    validate_analog_input_less()
+
+
+    initialize_switch_off()
+
+
 
     time.sleep(2)
-    
-    validate_analog_input_greater()
+
+
+
+    wififoff_toggle_button_toggle_switch()
 
     
 
